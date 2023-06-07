2d44a5a1522de37ee6587b8c39644b61dbba9f29
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-06-07 09:16:21 +0100

    completed hero

--- venom-hackathon/client-side/venom-hackathon-frontend/src/Pages/Landing/Landing.jsx
+++ venom-hackathon/client-side/venom-hackathon-frontend/src/Pages/Landing/Landing.jsx
@@ -2,13 +2,20 @@ import React, { useState } from 'react'
 import './Landing.scss'
 import Navbar from '../../Components/Navbar/Navbar'
 import Input from '../../Components/Input/Input'
+import Hero from '../../Components/Hero/Hero'
 
 const Landing = () => {
 
   return (
     <div id='landing'>
-        Landing
-        <Navbar />
+
+      <section>
+          <Navbar />
+
+          <Hero />
+      </section>
+
+      
     </div>
   )
 }
